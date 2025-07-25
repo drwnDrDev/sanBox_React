90c5af8f27fa4366757c06bc3debaf346e3edd86
DrwnDrDev <drwn.dr.dev@gmail.com>
2025-07-25 12:08:35 -0500

    componente ODS

--- src/App.js
+++ src/App.js
@@ -1,50 +1,9 @@
 import { useState } from "react";
+import { FormPaciente } from "./OrdenDeServicio/formPaciente";
 
 export default function App() {
-    const [users, setUsers] = useState([]);
-    const [formData, setFormData] = useState({ name: "", email: "" });
-
-    const handleSave=()=>{
-        if (!formData.name || !formData.email) return;
-        const newUser = {...formData};
-        setUsers([...users, newUser]);
-        setFormData({name:"", email: ""});
-    }
-
-    const handleDel=(index)=>{
-        const updated = users.filter((_, i) => i !== index);
-        setUsers(updated);
-    }
-
-    return (
-        <div className="w-1/2 m-auto border border-stone-600 p-4">
-            <div>
-                <input
-                    type="text"
-                    placeholder="Nombre"
-                    value={formData.name}
-                    onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-                    className="border rounded px-3 py-2 w-full mb-2"
-                />
-                <input
-                    type="email"
-                    placeholder="email"
-                    value={formData.email}
-                    onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-                    className="border rounded px-3 py-2 w-full mb-2"
-                />
-                <button onClick={handleSave} className="px-6 py-2 m-auto bg-purple-200 rounded-xl">Agregar</button>
-            </div>
-            <ul>
-                {users.map((user, index)=>(
-                
-                <li key={index}>
-                        <span>{user.name} - {user.email}</span>
-                        <button onClick={()=>handleDel(index)} className="text-red-500 font-bold">Del</button>
-                </li>
-                ))}
-            </ul>
-        </div>
-    );
-
+    
+return(
+    <FormPaciente/>
+)
 }
